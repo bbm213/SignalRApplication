464805287d19a5d0f29fcdcc946b8f229e67fbaf
BEN BRAHIM Mohamed El Amine <59963935+bbm213@users.noreply.github.com>
2021-01-28 15:51:10 +0100

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRApplication.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string fromUser, string message) 
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, message);
+        }
+    }
+}
